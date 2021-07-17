b548ef7f344ca2414f35fdbc8c12a1012bccbbaf
Remulo Costa <remulo.costa@vulcanet.com.br>
2021-07-17 15:30:17 -0300

    Create dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import { Summary } from '../Summary';
+
+import * as Styles from './styles';
+
+
+function Dashboard() {
+  return (
+    <Styles.Container>
+      <Summary />
+    </Styles.Container>
+  );
+}
+
+
+export {
+  Dashboard
+}
